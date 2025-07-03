d98ac7bdc7e602b10fb43574fb3f48a4674ffdfc
Hector Villasenor <hvillasenorch@icloud.com>
2025-07-03 13:34:01 -0700

    Enable MySQL strict mode

--- agentcpq/settings.py
+++ agentcpq/settings.py
@@ -121,6 +121,10 @@ WSGI_APPLICATION = "agentcpq.wsgi.application"
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
+
+DATABASES['default']['OPTIONS'] = {
+    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
