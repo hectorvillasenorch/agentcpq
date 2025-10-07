94bd27aea9799f3ca340d13f5e6e11c20f3cc859
Hector Villasenor <hvillasenorch@icloud.com>
2025-10-07 16:46:20 -0700

    static files added in settings

--- agentcpq/settings.py
+++ agentcpq/settings.py
@@ -177,11 +177,21 @@ USE_I18N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 STATICFILES_DIRS = [
      BASE_DIR / 'static'
 ]
+
+
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#      BASE_DIR / 'static'
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
