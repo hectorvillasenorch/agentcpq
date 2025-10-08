85619157d936f8250ff09bac630e14d6cf579967
Hector Villasenor <hvillasenorch@icloud.com>
2025-10-07 18:00:58 -0700

    updated greeting - added username

--- static/js/agents/agent_main.js
+++ static/js/agents/agent_main.js
@@ -64,7 +64,7 @@ function renderGreeting() {
   if (!chatBox || chatBox.children.length > 0) return;
 
 const greetingText = `
-👋 <b>Hello and welcome to AgentCPQ!</b><br><br>
+👋 <b>Hello ${userName} and welcome to AgentCPQ!</b><br><br>
 I’m here to make quoting simpler than ever.<br><br>
 You’ll notice there’s no traditional UI full of forms, buttons, or menus — that’s intentional. Everything happens right here, in one place. No clutter. No wiki. Just ask, and I’ll handle it for you.<br><br>
 
