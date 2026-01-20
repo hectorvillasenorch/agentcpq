1929642a9faaf670605b995d569914c5eb21ecbc
Hector Villasenor <hvillasenorch@icloud.com>
2026-01-19 17:39:25 -0800

    callback issue

--- salesforce/urls.py
+++ salesforce/urls.py
@@ -4,6 +4,7 @@ from salesforce.views import salesforce_login, salesforce_callback, token_receiv
 urlpatterns = [
     path("login/", salesforce_login, name="salesforce_login"),
     path("callback/", salesforce_callback, name="salesforce_callback"),
+    path("callback", salesforce_callback, name="salesforce_callback_noslash"),
     path("token-receiver/", token_receiver, name="token_receiver"),
     path("test-api/", test_salesforce_api, name="test_salesforce_api"),
     path("sync-quote/<int:quote_id>/", sync_quote_to_salesforce, name="sync_quote_to_salesforce")
